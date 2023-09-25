fb07bb52e15101f51ea1d456cd156f1ec9144779
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-09-25 16:07:08 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Challenge 1: Create a variable called multiply that will multiply num1 and num2 to get 62.
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Challenge 2: Create a variable called random that generates a random integer greater than 0.
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Challenge 3: Create a variable called mod that calculates the remainder of dividing num3 by num4; the remainder should be 4.
+const num3 = 64;
+const num4 = 60;
+const mod = num3 % num4;
+
+// Challenge 4: Create a variable called max that finds the highest number in a set; the value returned should be 20.
+const max = Math.max(1, 3, 5, 7, 11, 20);
